be97b7073fcbe5fa0d9fb5c445cd3c7aa3bb6c6b
Alexander <52704151+PIPSKVIK@users.noreply.github.com>
2020-10-12 20:43:12 +0300

    Getting started with the slider

---
+++ src/js/slider.js
@@ -0,0 +1,16 @@
+require('slick-carousel');
+require('slick-carousel/slick/slick.css');
+require('slick-carousel/slick/slick-theme.css');
+const $ = require('jquery');
+
+$(document).ready(function(){
+  $('.slider').slick({
+    infinite: true,
+    dots: true,
+    dotsClass: "promo-slider__dots",
+    autoplay: false,
+    autoplaySpeed: 2000,
+    fade: true,
+    arrows: false
+  });
+});
